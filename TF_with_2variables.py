ca61b649b0c239ff719b02362df673fc0fdc6331
nat <arslan.natali@gmail.com>
2017-09-26 14:19:31 +0200

    Update TF_with_2variables.py

--- TF_with_2variables.py
+++ TF_with_2variables.py
@@ -20,29 +20,30 @@ display_step = 10
 columns = defaultdict(list)
 
 data = pandas.read_csv(tf.gfile.Open(DATA_PATH), names=COLUMNS, delimiter=',', header=0)
-#data = pandas.read_csv("blocket_scrape.csv", names=COLUMNS, delimiter=',', header=0)  
 
-x1 = np.array(columns['size'], dtype=np.float)
-x2 = np.array(columns['room'], dtype=np.float)
-x = tf.Variable(np.row_stack((x1, x2)).astype(np.float32))
-y = np.array(columns['price'], dtype=np.float)
+x1 = tf.Variable(np.array(columns['size']).astype(np.float32))
+x2 = tf.Variable(np.array(columns['room']).astype(np.float32))
+y = tf.Variable(np.array(columns['price']).astype(np.float32))
 
+train_X1 = np.asarray([i[1] for i in data.loc[:,['size']].to_records()],dtype="float")
+train_X2 = np.asarray([i[1] for i in data.loc[:,['room']].to_records()],dtype="float")
 train_X = np.asarray([i[1] for i in data.loc[:,'size':'room'].to_records()],dtype="float")
 train_Y = np.asarray([i[1] for i in data.loc[:,['price']].to_records()],dtype="float")
 
-print (train_X.shape)
-print (train_Y.shape)
-
-
 n_samples = train_X.shape[0]
 
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+X1 = tf.placeholder("float")
+X2 = tf.placeholder("float")
+Y = tf.placeholder("float")
 
-W = tf.Variable(rng.randn(), [1,2])
+W1 = tf.Variable(rng.randn(), name="weight1")
+W2 = tf.Variable(rng.randn(), name="weight2")
 b = tf.Variable(rng.randn(), name="bias")
 
-pred = tf.add(tf.multiply(X, W), b)
+#Construct Linear Model
+sum_list = [tf.multiply(X1,W1),tf.multiply(X2,W2)] #pred = W1*X1+W2*X2+W3*X3+b
+pred_X = tf.add_n(sum_list)
+pred = tf.add(pred_X,b)
 
 cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
 
@@ -57,24 +58,30 @@ with tf.Session() as sess:
  
     # Fit all training data
     for epoch in range(training_epochs):
-        for (x, y) in zip(train_X, train_Y):
-            sess.run(optimizer, feed_dict={X: x, Y: y})
+        for (x1, x2, y) in zip(train_X1, train_X2, train_Y):
+            sess.run(optimizer, feed_dict={X1: x1, X2:x2, Y: y})
  
         #Display logs per epoch step
         if (epoch+1) % display_step == 0:
-            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-            print( "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),"W=", sess.run(W), "b=", sess.run(b))
+            c = sess.run(cost, feed_dict={X1:train_X1 , X2:train_X2, Y: train_Y})
+            print( "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),"W1=", sess.run(W1), "W2=", sess.run(W2), "b=", sess.run(b))
  
     print("Optimization Finished!")
-    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+    training_cost = sess.run(cost, feed_dict={X1:train_X1 , X2:train_X2, Y: train_Y})
+    print("Training cost=", training_cost, "W1=", sess.run(W1), "W2=", sess.run(W2), "b=", sess.run(b), '\n')
     
-    print ("W=",sess.run(W))
+    print ("W1=",sess.run(W1))
+    print ("W2=",sess.run(W2))
     print ("b=",sess.run(b))
- 
+
     #Graphic display
-    plt.plot(train_X, train_Y, 'ro', label='Original data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    #plt.plot(train_X1, train_Y, 'ro', label='Original X1', color='green')
+    #plt.plot(train_X2, train_Y, 'ro', label='Original X2', color='blue')
+    plt.plot(train_X, train_Y, 'ro', label='Original X2', color='blue')
+    plt.plot(train_X, sess.run(W1) * train_X1 + sess.run(W2) * train_X2 + sess.run(b), 
+             label='Fitted line',color='orange' )
     plt.legend()
     plt.show()
-    
+
+
+
